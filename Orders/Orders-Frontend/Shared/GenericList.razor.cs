69c65275ac4182740ef0a44e1ccc4f2b80fbf676
BuritiCrack <jmbur@BURITICRACK>
2025-04-04 13:30:12 -0500

    My frist generic component in Razor

---
+++ Orders/Orders-Frontend/Shared/GenericList.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Orders_Frontend.Shared
+{
+    public partial class GenericList<Titem>
+    {
+        [Parameter] public RenderFragment? Loading { get; set; }
+        [Parameter] public RenderFragment? NoRecord { get; set; }
+        [Parameter, EditorRequired] public RenderFragment Body { get; set; } = null!;
+        [Parameter, EditorRequired] public List<Titem> MyList { get; set; }
+    }
+}
