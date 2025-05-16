99adc2b6257f18aac7caad5f496e11588ae42f29
BuritiCrack <jmbur@BURITICRACK>
2025-05-16 17:02:49 -0500

    ChangePassword implemented

---
+++ Orders/Orders-Frontend/Pages/Auth/ChangePassword.razor.cs
@@ -0,0 +1,44 @@
+using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Orders_Frontend.Repositories;
+using Orders_Frontend.Shared;
+using Orders_Shared.DTOs;
+
+namespace Orders_Frontend.Pages.Auth
+{
+    public partial class ChangePassword
+    {
+        private ChangePasswordDTO changePasswordDTO = new();
+        private bool loading;
+
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        [Inject] private IRepository Repository { get; set; } = null!;
+
+        private async Task ChangePsswordAsync()
+        {
+            loading = true;
+            var responseHttp = await Repository.PostAsync("api/accounts/changePassword", changePasswordDTO);
+            loading = false;
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error",message, SweetAlertIcon.Error);
+                loading = false;
+                return;
+            }
+
+            loading = true;
+            NavigationManager.NavigateTo("/editUser");
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Contraseña cambiana con éxito.");
+        }
+    }
+}
---
+++ Orders/Orders-Shared/DTOs/ChangePasswordDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders_Shared.DTOs
+{
+    public class ChangePasswordDTO
+    {
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña actual")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "El campo {0} debe de tener entre {2} y {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string CurrentPassword { get; set; } = null!;
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña actual")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "El campo {0} debe de tener entre {2} y {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string NewPassword { get; set; } = null!;
+
+        [Compare("NewPassword", ErrorMessage = "Las contraseñas no coinciden.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña actual")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "El campo {0} debe de tener entre {2} y {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Confirm { get; set; } = null!;
+    }
+}
