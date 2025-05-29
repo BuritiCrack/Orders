2dd37ceb5e459f2c3609b564355c14dd1f7b3788
BuritiCrack <jmbur@BURITICRACK>
2025-05-29 17:29:28 -0500

    Add product management entities and database schema
    Introduce `Product`, `ProductCategory`, and `ProductImage` entities in the `Orders_Backend` project. Update `DataContext.cs` to include new `DbSet` properties and create unique indexes for products. Modify `DataContextModelSnapshot.cs` to reflect the new entities and their configurations. Create migration `AddProductEntities` to establish the corresponding database tables. Enhance `Category.cs` with a navigation property for product categories. Overall, these changes improve the data model for better product management capabilities.

--- Orders/Orders-Backend/Data/DataContext.cs
+++ Orders/Orders-Backend/Data/DataContext.cs
@@ -13,12 +13,17 @@ namespace Orders_Backend.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<State> States { get; set; }
         public DbSet<City> Cities { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Country>().HasIndex(i => i.Name).IsUnique();
             modelBuilder.Entity<Category>().HasIndex(i => i.Name).IsUnique();
+            modelBuilder.Entity<Product>().HasIndex(x => x.Name).IsUnique();
             modelBuilder.Entity<State>().HasIndex(i => new {i.CountryId, i.Name}).IsUnique();
             modelBuilder.Entity<City>().HasIndex(i => new {i.StateId, i.Name}).IsUnique();
             DisableCascadingDelete(modelBuilder);
--- Orders/Orders-Shared/Entities/Category.cs
+++ Orders/Orders-Shared/Entities/Category.cs
@@ -11,5 +11,11 @@ namespace Orders_Shared.Entities
         [Required(ErrorMessage = "El campo {0} no puede estar vacio.")]
         [MaxLength(30, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
         public string Name { get; set; } = null!;
+
+        public ICollection<ProductCategory>? ProductCategories { get; set; }
+
+        [Display(Name = "Productos")]
+        public int ProductCategoriesNumber => ProductCategories == null || ProductCategories.Count == 0 ? 0 : ProductCategories.Count;
+
     }
 }
